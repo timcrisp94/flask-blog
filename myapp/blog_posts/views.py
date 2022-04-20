0b8a5e0f80987dacf4f44142a94d89a43fb03f6a
Tim Crisp <timothycrisp@gmail.com>
2022-04-20 15:32:08 -0500

    create blog route

--- myapp/blog_posts/views.py
+++ myapp/blog_posts/views.py
@@ -6,3 +6,15 @@ from myapp.blog_posts.forms import BlogPostForm
 
 blog_posts = Blueprint('blog_posts', __name__)
 
+@blog_posts.route('/create', methods=['GET', 'POST'])
+@login_required
+def create_post():
+    form = BlogPostForm()
+    if form.validate_on_submit():
+        blog_post = BlogPost(title=form.title.data, text=form.text.data, user_id=current_user.id)
+        db.session.add(blog_post)
+        db.session.commit()
+        flash('Blog Post Created')
+        print('Blog post was created')
+        return redirect(url_for('core.index'))
+    return render_template('create_post.html', form=form)
